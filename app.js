b4f44e682bdc1bb458009d3beb6a9da18dcbf065
jaeheonkim0913 <jaeheonkim0913@gmail.com>
2022-11-21 12:41:47 -0500

    fix

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-import express from 'express'
-import cors from 'cors'
-import HelloController
-    from "./controllers/hello-controller.js"
-import UserController
-    from "./controllers/users/users-controller.js"
-import TuitsController
-    from "./controllers/tuits/tuits-controller.js";
-const app = express()
+import express from "express";
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors())
 TuitsController(app);
-HelloController(app)
-UserController(app)
-app.listen(process.env.PORT || 4000)
+HelloController(app);
+UserController(app);
+app.listen(process.env.PORT || 4000);
