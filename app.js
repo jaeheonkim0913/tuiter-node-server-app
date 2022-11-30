a218357faa7201f51eb250035347354c0f9c7d05
jaeheonkim0913 <jaeheonkim0913@gmail.com>
2022-11-30 02:22:32 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://localhost:27017/tuiter');
+const DB_CONNECTION_STRING = 'mongodb+srv://jaeheonkim0913:jPeUpfH68o2mI80b@cluster0.pplfvtk.mongodb.net/tuiter?retryWrites=true&w=majority'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
